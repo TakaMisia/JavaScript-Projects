25dbe6573f3876742703c0c5114944a325779a7f
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2021-10-31 17:13:13 +0000

    Add files via upload

---
+++ Project3_math_operators/index.js
@@ -0,0 +1,6 @@
+<script>
+    function more_Math(){
+    var simple_Math= (2 + 2) * 10 / 4 -3 ;
+    document.getElementsById ("Math") .innerHTML = "2 plus 2, multiplied by 10, divided by 4 and then substract by 3 " + simple_Math;
+}
+</script>
---
+++ Project3_math_operators/main.js
@@ -0,0 +1,26 @@
+// basic math
+function addition_Function (){
+    var addition = 2 + 2 ;
+    document.getElementsById ("Math") .innerHTML = "2 + 2 =" + addition ;
+}
+
+    function substraction_Function (){
+    var substraction= 2 - 2 ;
+    document.getElementsById ("Math") .innerHTML = "2 - 2 =" + substraction;
+}
+function multiplication_Function (){
+    var simple_Math= 2 * 2 ;
+    document.getElementsById ("Math") .innerHTML = "2 * 2 =" + simple_Math;
+}
+function division_Function (){
+    var simple_Math= 2 / 2 ;
+    document.getElementsById ("Math") .innerHTML = "2 / 2 =" + simple_Math;
+}
+function multiple_function(){
+    var simple_Math= (2 + 2) * 10 / 4 -3 ;
+    document.getElementsById ("Math") .innerHTML = "Results for multiples function will be: " + simple_Math;
+}
+function modulus_Operator (){
+    var simple_Math= 25% 6;
+    document.getElementsById ("Math") .innerHTML = "Results: " + simple_Math;
+}
