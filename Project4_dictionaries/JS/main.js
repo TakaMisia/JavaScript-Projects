4cac589c35fa1941f492242c7586284586533d33
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2021-11-02 16:01:15 +0000

    Add files via upload

--- Project4_dictionaries/JS/main.js
+++ Project4_dictionaries/JS/main.js
@@ -1,30 +1,44 @@
 // basic math
 function addition_Function (){
     var addition = 2 + 2 ;
-    document.getElementsById ("Dictionary") .innerHTML = "2 + 2 =" + addition ;
+    document.getElementById ("Dictionary") .innerHTML = "2 + 2 =" + addition ;
 }  ;
 
     function substraction_Function (){
     var substraction= 2 - 2 ;
-    document.getElementsById ("Dictionary") .innerHTML = "2 - 2 =" + substraction;
+    document.getElementById ("Dictionary") .innerHTML = "2 - 2 =" + substraction;
 } 
 
 function multiplication_Function (){
     var simple_Math= 2 * 2 ;
-    document.getElementsById ("Dictionary") .innerHTML = "2 * 2 =" + simple_Math;
+    document.getElementById ("Dictionary") .innerHTML = "2 * 2 =" + simple_Math;
 }
 
 function division_Function (){
     var simple_Math= 2 / 2 ;
-    document.getElementsById ("Dictionary") .innerHTML = "2 / 2 =" + simple_Math;
+    document.getElementById ("Dictionary") .innerHTML = "2 / 2 =" + simple_Math;
 } 
 
 function multiple_function(){
     var simple_Math= (2 + 2) * 10 / 4 -3 ;
-    document.getElementsById ("Dictionary") .innerHTML = "Results for multiples function will be: " + simple_Math;
+    document.getElementById ("Dictionary") .innerHTML = "Results for multiples function will be: " + simple_Math;
  }
 
 function modulus_Operator (){
     var simple_Math= 25% 6;
-    document.getElementsById ("Math") .innerHTML = "Results: " + simple_Math;
+    document.getElementById ("Dictionary") .innerHTML = "Results: " + simple_Math;
 } 
+function random() {
+    document.getElementById("Dictionary").innerHTML = Math.random(); 
+}
+
+function plus() {
+    var a = 5;
+    a++;
+    document.getElementById("Dictionary").innerHTML = a; 
+}
+function minus() {
+    var b = 3;
+    b--;
+    document.getElementById("Dictionary").innerHTML = b; 
+}
