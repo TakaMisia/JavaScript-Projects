43486b1176aa5084e6e25a421070962c69c9b628
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2021-11-02 09:40:33 +0000

    Add files via upload

---
+++ Project1_expressions_alert/Project2_functions.js
@@ -0,0 +1,9 @@
+<script>
+    var age = myfunction(23, 32) ;
+   //23 my  age  32 piotr age 
+    document.getElementById("p1").innerHTML = age;
+
+    function myfunction (a, b) {
+        return a+=b;
+    }
+</script>
