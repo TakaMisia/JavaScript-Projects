d79dc312a4241bce51f7d3b5c97e796086bbc5b5
Michalina Brodnicka <88375452+TakaMisia@users.noreply.github.com>
2021-11-02 09:57:10 +0000

    Add files via upload

---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,30 @@
+// basic math
+function addition_Function (){
+    var addition = 2 + 2 ;
+    document.getElementsById ("Dictionary") .innerHTML = "2 + 2 =" + addition ;
+} ;
+
+    function substraction_Function (){
+    var substraction= 2 - 2 ;
+    document.getElementsById ("Dictionary") .innerHTML = "2 - 2 =" + substraction;
+} ;
+
+function multiplication_Function (){
+    var simple_Math= 2 * 2 ;
+    document.getElementsById ("Dictionary") .innerHTML = "2 * 2 =" + simple_Math;
+}
+
+function division_Function (){
+    var simple_Math= 2 / 2 ;
+    document.getElementsById ("Dictionary") .innerHTML = "2 / 2 =" + simple_Math;
+} ;
+
+function multiple_function(){
+    var simple_Math= (2 + 2) * 10 / 4 -3 ;
+    document.getElementsById ("Dictionary") .innerHTML = "Results for multiples function will be: " + simple_Math;
+ ;}
+
+function modulus_Operator (){
+    var simple_Math= 25% 6;
+    document.getElementsById ("Math") .innerHTML = "Results: " + simple_Math;
+} ;
